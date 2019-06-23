5b0470dbe800876c4de99c494eba9823dd525adb
nowfel terki <nowfel.terki@tutanota.com>
2019-06-23 13:30:22 +0200

    add function to count and copy longest line

---
+++ chap1/1-9.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000 
+
+int get_line(char *line, int maxline);
+void copy(char *longest, char line[]);
+
+int main(int argc, char *argv[])
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+    while((len = get_line(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        } 
+    }
+    if (max > 0)
+        printf("%s", longest);
+
+    printf("\n!CIAO!\n");
+    return EXIT_SUCCESS;
+}
+
+
+int get_line(char *s, int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy(char *to, char *from)
+{
+    int i;
+    i = 0;
+    while((to[i] = from[i]) != '\0')
+        ++i;
+}
